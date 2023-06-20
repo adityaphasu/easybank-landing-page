84ad159346de896b4e030d475617685eb4d01106
Aditya Phasu <adityaphasu919@gmail.com>
2023-06-20 22:43:53 +0530

    refactor: unordered lists and footer socials

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,49 +4,40 @@ import Logo from "../../assets/logo2.svg";
 import { Facebook, Youtube, Twitter, Pinterest, Instagram } from "../../assets/icons/icons";
 import "./Footer.scss";
 
+const socialMediaData = [
+  { label: "facebook", image: Facebook },
+  { label: "youtube", image: Youtube },
+  { label: "twitter", image: Twitter },
+  { label: "pinterest", image: Pinterest },
+  { label: "instagram", image: Instagram },
+];
+
+const footerLinks = ["About Us", "Contact", "Blog", "Careers", "Support", "Privacy Policy"];
+
 export default function Footer() {
   return (
     <footer className="footer">
       <div>
         <img src={Logo} alt="Easybank" className="footer__logo" />
         <div className="footer__socials">
-          <a href="#" aria-label="Facebook" className="footer__social">
-            {<img src={Facebook} alt="" />}
-          </a>
-          <a href="#" aria-label="Youtube" className="footer__social">
-            <img src={Youtube} alt="" />
-          </a>
-          <a href="#" aria-label="Twitter" className="footer__social">
-            <img src={Twitter} alt="" />
-          </a>
-          <a href="#" aria-label="Pinterest" className="footer__social">
-            <img src={Pinterest} alt="" />
-          </a>
-          <a href="#" aria-label="Instagram" className="footer__social">
-            <img src={Instagram} alt="" />
-          </a>
+          {socialMediaData.map((socialMedia) => (
+            <a
+              href="#"
+              aria-label={socialMedia.label}
+              key={socialMedia.label}
+              className="footer__social">
+              <img src={socialMedia.image} alt="" />
+            </a>
+          ))}
         </div>
       </div>
       <nav className="footer__navbar" aria-label="secondary navbar">
         <ul className="footer__list">
-          <li className="footer__item">
-            <a href="#">About Us</a>
-          </li>
-          <li className="footer__item">
-            <a href="#">Contact</a>
-          </li>
-          <li className="footer__item">
-            <a href="#">Blog</a>
-          </li>
-          <li className="footer__item">
-            <a href="#">Careers</a>
-          </li>
-          <li className="footer__item">
-            <a href="#">Support</a>
-          </li>
-          <li className="footer__item">
-            <a href="#">Privacy Policy</a>
-          </li>
+          {footerLinks.map((text, index) => (
+            <li key={index} className="footer__item">
+              <a href="#">{text}</a>
+            </li>
+          ))}
         </ul>
       </nav>
       <div className="footer__container">
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,8 @@ import "./Header.scss";
 import Logo from "../../assets/logo.svg";
 import Button from "../Button/Button";
 
+const headerLinks = ["Home", "About", "Contact", "Blog", "Careers"];
+
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -27,31 +29,13 @@ export default function Header() {
             <span className="header__span"></span>
           </button>
           <ul className={`header__list ${isMenuOpen ? "header__list--open" : ""}`}>
-            <li className="header__item">
-              <a href="#" className="header__link">
-                Home
-              </a>
-            </li>
-            <li className="header__item">
-              <a href="#" className="header__link">
-                About
-              </a>
-            </li>
-            <li className="header__item">
-              <a href="#" className="header__link">
-                Contact
-              </a>
-            </li>
-            <li className="header__item">
-              <a href="#" className="header__link">
-                Blog
-              </a>
-            </li>
-            <li className="header__item">
-              <a href="#" className="header__link">
-                Careers
-              </a>
-            </li>
+            {headerLinks.map((text, index) => (
+              <li key={index} className="header__item">
+                <a href="#" className="header__link">
+                  {text}
+                </a>
+              </li>
+            ))}
           </ul>
           <Button className="btn btn--hidden" />
         </nav>
